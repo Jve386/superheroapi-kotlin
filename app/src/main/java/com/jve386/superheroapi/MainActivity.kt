50eb10f555e4e04bd5fc5b1aaff0d3067d9c27ea
Jve386 <jve.formacio@gmail.com>
2023-12-31 16:43:48 +0100

    Fixed libraries bug with newer AndroidStudio version. Added: SearchView and recycler view

--- app/src/main/java/com/jve386/superheroapi/MainActivity.kt
+++ app/src/main/java/com/jve386/superheroapi/MainActivity.kt
@@ -15,32 +15,6 @@ import com.jve386.superheroapi.ui.theme.SuperheroapiTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SuperheroapiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SuperheroapiTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
